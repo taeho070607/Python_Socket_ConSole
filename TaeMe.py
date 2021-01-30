050ce5e174fbea81cca4dbf77e7711cf499084b9
taeho070607 <47946776+taeho070607@users.noreply.github.com>
2021-01-30 21:40:08 +0900

    Update TaeMe.py

--- TaeMe.py
+++ TaeMe.py
@@ -2,6 +2,10 @@ from time import sleep
 import socket
 from _thread import *
 import winsound
+from gtts import gTTS
+from playsound import playsound
+import tkinter as tk
+import os
 class TaeMe():
     class System:
         output = print
@@ -43,6 +47,10 @@ class TaeMe():
     def run(self,color: str="",ConSoleTime: float=0.125,ConSolePost: str="/",ConSoleValue: str="=",Host: str="127.0.0.1",Port: int=8080,Error: str=None,Server: str=None,Admin: bool=False):
         # 접속한 클라이언트마다 새로운 쓰레드가 생성되어 통신을 하게 됩니다.
         def threaded(client_socket, addr,color,ConSoleTime,ConSolePost,ConSoleValue,Error,Server,Admin):
+            connect = gTTS(text=f"{addr[0]}님이 접속했습니다.",lang="ko")
+            connect.save("connect.mp3")
+            playsound("connect.mp3")
+            os.remove("connect.mp3")
             ADMIN=False
             def print(value):
                 client_socket.sendall(value.encode())
@@ -213,11 +221,15 @@ class TaeMe():
                                 print("ConSoleError -> " + str(self.a.split(ConSoleValue)[1]))
                             elif "ServerByStatus" in self.a:
                                 self.event = True
-                                print("ServerByUsers(Online) = " + str(len(self.users))+"\n"+"ServerByBanUsers = "+str(len(self.ban_users))+"\n"+"ServerByAccumulateUsers = "+str(len(self.accumulate_users))+"\n"+"ServerByBanMessage = "+str(self.BanMessage)+"\n"+"ServerByFullUsersMessage = "+str(self.FullMessage))
+                                print("ServerByUsers(Online) = " + str(len(self.users))+"\n"+"ServerByBanUsers = "+str(len(self.ban_users))+"\n"+"ServerByAccumulateUsers = "+str(len(self.accumulate_users))+"\n"+"ServerByMaxUsers = "+str(self.max_users)+"\n"+"ServerByBanMessage = "+str(self.BanMessage)+"\n"+"ServerByFullUsersMessage = "+str(self.FullMessage))
                             elif "ServerByFullUsersMessage" in self.a:
                                 self.event = True
                                 self.FullMessage = str(self.a.split(ConSoleValue)[1])
                                 print("ServerByFullUsersMessage -> " + str(self.a.split(ConSoleValue)[1]))
+                            else:
+                                value = print(color + fun(content=self.a, color=color))
+                                if value is None:
+                                    print(" ")
                         else:
                             value = print(color + fun(content=self.a, color=color))
                             if value is None:
@@ -247,31 +259,78 @@ class TaeMe():
                     # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코)
                 except ConnectionResetError as e:
                     self.System.output('Disconnected by ' + addr[0], ':', addr[1])
+                    Wait = gTTS(text=f"{addr[0]}님이 접속을 종료했습니다.", lang='ko')
+                    Wait.save("disconnect.mp3")
+                    playsound("disconnect.mp3")
+                    os.remove("disconnect.mp3")
                     self.users.remove(addr[0])
                     break
 
             client_socket.close()
+        try:
+            HOST = Host
+            PORT = Port
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind((HOST, PORT))
+            server_socket.listen()
+            # 클라이언트가 접속하면 accept 함수에서 새로운 소켓을 리턴합니다.
+            ServerRun = gTTS(text='서버를 시작합니다.', lang='ko')
+            ServerRun.save("ServerRun.mp3")
+            playsound("ServerRun.mp3")
+            os.remove("ServerRun.mp3")
+            print("ServerRun ..........[O]")
+            # 새로운 쓰레드에서 해당 소켓을 사용하여 통신을 하게 됩니다.
+            try:
+                while True:
+                    Wait = gTTS(text='대기중', lang='ko')
+                    Wait.save("wait.mp3")
+                    playsound("wait.mp3")
+                    os.remove("wait.mp3")
+                    print("wait ..........[O]")
+                    client_socket, addr = server_socket.accept()
+                    start_new_thread(threaded, (client_socket, addr,color,ConSoleTime,ConSolePost,ConSoleValue,Error,Server,Admin))
+            except:
+                print("Error[02]: Connect Fail")
+                playsound("sound\\Nuclear.mp3")
 
-        HOST = Host
-        PORT = Port
-
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind((HOST, PORT))
-        server_socket.listen(1)
-
-        print('server start')
-
-        # 클라이언트가 접속하면 accept 함수에서 새로운 소켓을 리턴합니다.
-
-        # 새로운 쓰레드에서 해당 소켓을 사용하여 통신을 하게 됩니다.
-        while True:
-            print('wait')
-
-            client_socket, addr = server_socket.accept()
-            start_new_thread(threaded, (client_socket, addr,color,ConSoleTime,ConSolePost,ConSoleValue,Error,Server,Admin))
-
-        server_socket.close()
+            server_socket.close()
+        except:
+            print("Error[01]: ServerRun Fail")
+            playsound("sound\\Nuclear.mp3")
+    def display_run(self):
+        global run
+        run = False
+        calc = tk.Tk()
+        calc.geometry("300x300")
+        calc.title("Socket_ConSole")
+        def thread(event):
+            global run
+            if run:
+                return 0
+            else:
+                run = True
+                text.set("ServerStatus : True")
+            def fun(event):
+                self.run(self=self, Host=str(tk.Entry.get(display)), Port=int(tk.Entry.get(display2)),
+                           Server=str(tk.Entry.get(display)))
+            start_new_thread(fun, (event,))
+        display = tk.Entry(calc, width=20)
+        display2 = tk.Entry(calc, width=20)
+        button = tk.Button(calc, text="ServerStart", width=20)
+        label = tk.Label(calc, text="Host", width=10)
+        label2 = tk.Label(calc, text="Port", width=10)
+        text = tk.StringVar()
+        label3 = tk.Label(calc, textvariable=text)
+        text.set("ServerStatus : False")
+        button.bind('<Button-1>', thread)
+        label.pack()
+        display.pack()
+        label2.pack()
+        display2.pack()
+        label3.pack()
+        button.pack()
+        calc.mainloop()
     def client_run(self,Host: str="127.0.0.1",Port: int=8080):
         HOST = Host
         # 서버에서 지정해 놓은 포트 번호입니다.
@@ -286,15 +345,47 @@ class TaeMe():
             a = input(">")
             if a == "exit":
                 break
-            # 메시지를 전송합니다.
+             # 메시지를 전송합니다.
             client_socket.sendall(a.encode())
 
             # 메시지를 수신합니다.
             data = client_socket.recv(1024)
             print(data.decode())
 
-        # 소켓을 닫습니다.
+                # 소켓을 닫습니다.
         client_socket.close()
+    def display_client_run(self):
+        global run
+        run = False
+        calc = tk.Tk()
+        calc.geometry("300x300")
+        calc.title("Socket_ConSole")
+        def thread(event):
+            global run
+            if run:
+                return 0
+            else:
+                run = True
+                text.set("ConnectStatus : True")
+            def fun(event):
+                self.client_run(self=self, Host=str(tk.Entry.get(display)), Port=int(tk.Entry.get(display2)))
+            start_new_thread(fun, (event,))
+        display = tk.Entry(calc, width=20)
+        display2 = tk.Entry(calc, width=20)
+        button = tk.Button(calc, text="Connect", width=20)
+        label = tk.Label(calc, text="Host", width=10)
+        label2 = tk.Label(calc, text="Port", width=10)
+        text = tk.StringVar()
+        label3 = tk.Label(calc, textvariable=text)
+        text.set("ConnectStatus : False")
+        button.bind('<Button-1>', thread)
+        label.pack()
+        display.pack()
+        label2.pack()
+        display2.pack()
+        label3.pack()
+        button.pack()
+        calc.mainloop()
 class hello(TaeMe):
     None
 if __name__ == '__main__':
